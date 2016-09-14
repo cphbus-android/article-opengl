377439ed91618530a835bb37142342ca4b33d019
Anders Kalhauge <anders@kalhauge.dk>
2016-09-14 11:20:17 +0200

    Button added to main view

--- app/src/main/java/dk/cphbusiness/masterdetailtemplate/MainActivity.kt
+++ app/src/main/java/dk/cphbusiness/masterdetailtemplate/MainActivity.kt
@@ -2,12 +2,18 @@ package dk.cphbusiness.masterdetailtemplate
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        }
+
+    fun buttonPressed(view: View) {
+        Toast.makeText(this, "Pressed", Toast.LENGTH_LONG).show()
         }
 
     }
