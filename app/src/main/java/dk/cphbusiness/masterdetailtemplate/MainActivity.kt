8dcba370a15411210a0f5f3dd963b5761c20f406
Anders Kalhauge <anders@kalhauge.dk>
2016-09-21 09:25:03 +0200

    times extension method

--- app/src/main/java/dk/cphbusiness/masterdetailtemplate/MainActivity.kt
+++ app/src/main/java/dk/cphbusiness/masterdetailtemplate/MainActivity.kt
@@ -7,8 +7,16 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 
+fun String.times(count: Int) : String {
+    var result = this;
+    for (i in 2..count) {
+        result += this
+        }
+    return result
+    }
+
 fun Context.toast(message: String) {
-    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+    Toast.makeText(this, message.times(3), Toast.LENGTH_LONG).show()
     }
 
 class MainActivity : AppCompatActivity() {
